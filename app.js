2e8e179650dd4c65829a01a24eb5d5d6703ff26a
PRAJAPATI <pratik.a.prajapati@accenture.com>
2021-07-13 14:19:35 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app=express();
+app.get("/",function(request,response){
+    response.send("Hi there");
+});
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server has started");
+});
